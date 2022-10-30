c3b839e116579c5499141bdc5da61784c65bd7bc
Andrew <andrew.levik@gmail.com>
2022-10-30 16:21:59 +0100

    update loadmore-btn

--- src/index.js
+++ src/index.js
@@ -41,15 +41,17 @@ btnSearch.addEventListener('click', event => {
   const trimmedValue = input.value.trim();
   if (trimmedValue !== '') {
     fetchImages(trimmedValue, pageNumber).then(foundData => {
-      if (foundData.hits.length === 0) {
+      if (foundData.hits.length === 0) {  
+        console.log(foundData)
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       } else {
         renderImageList(foundData.hits);
-        Notiflix.Notify.success(
+        console.log(foundData)
+         Notiflix.Notify.success(
           `Hooray! We found ${foundData.totalHits} images.`
-        );
+          );
         btnLoadMore.style.display = 'block';
         gallerySimpleLightbox.refresh();
       }
@@ -61,17 +63,18 @@ btnLoadMore.addEventListener('click', event => {
   event.preventDefault();
   pageNumber += 1;
   const trimmedValue = input.value.trim();
-  btnLoadMore.style.display = 'none';
+  // btnLoadMore.style.display = 'none';
   fetchImages(trimmedValue, pageNumber).then(foundData => {
     if (foundData.hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-    } else {
-      renderImageList(foundData.hits);
-      Notiflix.Notify.success(
-        `Hooray! We found ${foundData.totalHits} images.`
-      );
+    }  else {
+      renderImageList(foundData.hits); 
+      console.log(foundData)
+      // Notiflix.Notify.success(
+      //   `Hooray! We found ${foundData.totalHits} images.`
+      // );
       btnLoadMore.style.display = 'block';
       gallerySimpleLightbox.refresh();
     }
@@ -111,8 +114,8 @@ function cleanGallery() {
   btnLoadMore.style.display = 'none';
 }
 
-gallery.addEventListener('keydown', event => {
-  if (event.key === 'Escape') {
-    instance.close();
-  }
-});
+// gallery.addEventListener('keydown', event => {
+//   if (event.key === 'Escape') {
+//     instance.close();
+//   }
+// });
