eb923493e561a69b416de85e90f5747334a09c5f
Andrew <andrew.levik@gmail.com>
2022-11-02 01:42:53 +0100

    edit async-await sintax

--- src/index.js
+++ src/index.js
@@ -1,98 +1,112 @@
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { fetchImages } from './js/axios-fetch';
+import fetchImages from './fetch-images.js';
 
 const input = document.querySelector('.search-form-input');
 const btnSearch = document.querySelector('.search-form-button');
-const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
 
-let gallerySimpleLightbox = new SimpleLightbox('.gallery a', {
-  captions: false,
-});
+const gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
 let pageNumber = 1;
 
 btnLoadMore.style.display = 'none';
 
-btnSearch.addEventListener('click', event => {
-  event.preventDefault();
-  cleanGallery();
-  const trimmedValue = input.value.trim();
-  if (trimmedValue !== '') {
-    fetchImages(trimmedValue, pageNumber).then(({ data }) => {
-      if (data.totalHits === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        renderImageList(data.hits);
-        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        btnLoadMore.style.display = 'block';
-        gallerySimpleLightbox.refresh();
-      }
-    });
-  }
-});
+input.addEventListener('submit', onFormSubmit);
+btnLoadMore.addEventListener('click', onLoadMore);
 
-btnLoadMore.addEventListener('click', event => {
-  event.preventDefault();
-  pageNumber += 1;
-  const trimmedValue = input.value.trim();
-  btnLoadMore.style.display = 'none';
-  fetchImages(trimmedValue, pageNumber).then(({ data }) => {
-    if (data.totalHits === 0) {
+async function onFormSubmit(e) {
+  
+  e.preventDefault();
+  pageNumber = 1;
+  gallery.innerHTML = '';
+  const trimmedValue = btnSearch.value.trim();
+
+  if (trimmedValue === '') {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  } else {
+    const data = await fetchImages(trimmedValue, pageNumber);
+
+    if (data.hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     } else {
-      renderImageList(data.hits);
-
-      const totalPages = Math.ceil(data.totalHits / 40);
-
-      btnLoadMore.style.display = 'block';
-      gallerySimpleLightbox.refresh();
-
-      if (pageNumber >= totalPages) {
-        btnLoadMore.style.display = 'none';
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
+      renderGallery(data.hits);
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      btnLoadMore.classList.remove('is-hidden');
     }
-  });
-});
+  }
+}
 
-function renderImageList(images) {
+async function onLoadMore() {
+  pageNumber += 1;
+  const trimmedValue = btnSearch.value.trim();
+  
+  const data = await fetchImages(trimmedValue, pageNumber);
+
+  if (data.hits.length === 0) {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+    btnLoadMore.classList.add('is-hidden');
+  } else {
+    renderGallery(data.hits);
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+    const totalPages = Math.ceil(data.totalHits / 40);
+
+    btnLoadMore.style.display = 'block';
+    gallerySimpleLightbox.refresh();
+
+    if (pageNumber >= totalPages) {
+      btnLoadMore.classList.add('is-hidden');
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+  }
+}
+
+function renderGallery(images) {
   console.log(images, 'images');
   const markup = images
-    .map(image => {
-      console.log('img', image);
-      return `<div class="photo-card">
-       <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        console.log('img', image);
+        return `<div class="gallery">
+       <a href="${largeImageURL}"><img class="photo" src="${webformatURL}" alt="${tags}" title="${tags}" loading="lazy"/></a>
         <div class="info">
            <p class="info-item">
-                <b>Likes</b> <span class="info-item-api">${image.likes}</span>
+                <b>Likes</b> <span class="info-item-api">${likes}</span>
             </p>
             <p class="info-item">
-                <b>Views</b> <span class="info-item-api">${image.views}</span>  
+                <b>Views</b> <span class="info-item-api">${views}</span>  
             </p>
             <p class="info-item">
-                <b>Comments</b> <span class="info-item-api">${image.comments}</span>  
+                <b>Comments</b> <span class="info-item-api">${comments}</span>  
             </p>
             <p class="info-item">
-                <b>Downloads</b> <span class="info-item-api">${image.downloads}</span> 
+                <b>Downloads</b> <span class="info-item-api">${downloads}</span> 
             </p>
         </div>
     </div>`;
-    })
+      }
+    )
     .join('');
   gallery.innerHTML += markup;
-}
-
-function cleanGallery() {
-  gallery.innerHTML = '';
-  pageNumber = 1;
-  btnLoadMore.style.display = 'none';
+  gallerySimpleLightbox.refresh();
 }
