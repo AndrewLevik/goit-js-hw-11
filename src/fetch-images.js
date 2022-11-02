a8c1242b482763709b414e89582b20968bcfed18
Andrew <andrew.levik@gmail.com>
2022-11-02 01:54:31 +0100

    update fetch-images.js

--- src/fetch-images.js
+++ src/fetch-images.js
@@ -3,11 +3,10 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '30935665-ea4f2fb7c578d2da022664c3d';
 
-async function fetchImages(inputValue, pageNumber) {
+export default async function fetchImages(inputValue, pageNumber) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&page=${pageNumber}&per_page=40`;
 
   return await axios.get(url);
    
 }
 
-export { fetchImages };
