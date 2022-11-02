22042dc81b92b9be3f5cce43f1f03b669ebf3a63
Andrew <andrew.levik@gmail.com>
2022-11-03 00:11:07 +0100

    final update

--- src/fetch-images.js
+++ src/fetch-images.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '30935665-ea4f2fb7c578d2da022664c3d';
 
 export default async function fetchImages(inputValue, pageNumber) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&page=${pageNumber}&per_page=40`;
-
-  return await axios.get(url);
-   
-}
-
+    const url = `${BASE_URL}?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&page=${pageNumber}&per_page=40`;
+    try {
+        const { data } = await axios.get(url);
+        return data;
+    } catch (error) {
+        Notiflix.Notify.failure('error');
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,50 +1,50 @@
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import fetchImages from './fetch-images.js';
-
-const form = document.querySelector('.search-form');
-const btnSearch = document.querySelector('.search-form-button');
-const btnLoadMore = document.querySelector('.load-more');
-const gallery = document.querySelector('.gallery');
-
-const gallerySimpleLightbox = new SimpleLightbox('.gallery a');
+import fetchImages from './fetch-images';
 
 let pageNumber = 1;
 
-btnLoadMore.style.display = 'none';
+const form = document.querySelector('.search-form');
+const input = document.querySelector('.search-form-input');
+const btnLoadMore = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
+
+let lightbox = new SimpleLightbox('.gallery a');
 
 form.addEventListener('submit', onFormSubmit);
 btnLoadMore.addEventListener('click', onLoadMore);
 
 async function onFormSubmit(e) {
-    e.preventDefault();
-    pageNumber = 1;
-    gallery.innerHTML = '';
-    const searchQuery = btnSearch.value.trim();
-
-    if (searchQuery === '') {
-        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-            return
-    } else {
-        const data = await fetchImages(searchQuery, pageNumber);
-
-        if (data.hits.length === 0) {
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-        } else {
-
-        renderGallery(data.hits);
-            Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-            btnLoadMore.classList.remove('is-hidden');
-    };
-}
-}
-
-async function onLoadMore(e) {
   e.preventDefault();
+  pageNumber = 1;
+  gallery.innerHTML = '';
+  const searchQuery = input.value.trim();
+
+  if (searchQuery === '') {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  } else {
+    const data = await fetchImages(searchQuery, pageNumber);
+
+    if (data.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      renderGallary(data.hits);
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      btnLoadMore.classList.remove('is-hidden');
+    }
+  }
+}
+
+async function onLoadMore() {
   pageNumber += 1;
-  const searchQuery = btnSearch.value.trim();
-  
+  const searchQuery = input.value.trim();
+
   const data = await fetchImages(searchQuery, pageNumber);
 
   if (data.hits.length === 0) {
@@ -53,26 +53,24 @@ async function onLoadMore(e) {
     );
     btnLoadMore.classList.add('is-hidden');
   } else {
-    renderGallery(data.hits);
-    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    renderGallary(data.hits);
 
     const totalPages = Math.ceil(data.totalHits / 40);
-
-    btnLoadMore.style.display = 'block';
-    gallerySimpleLightbox.refresh();
+    btnLoadMore.classList.remove('is-hidden');
 
     if (pageNumber >= totalPages) {
       btnLoadMore.classList.add('is-hidden');
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
+      setTimeout(() => {
+        Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }, 1000);
     }
   }
 }
 
-function renderGallery(images) {
-  console.log(images, 'images');
-  const markup = images
+function renderGallary(data) {
+  const markup = data
     .map(
       ({
         webformatURL,
@@ -83,27 +81,27 @@ function renderGallery(images) {
         comments,
         downloads,
       }) => {
-        console.log('img', images);
-        return `<div class="gallery">
-       <a href="${largeImageURL}"><img class="photo" src="${webformatURL}" alt="${tags}" title="${tags}" loading="lazy"/></a>
-        <div class="info">
-           <p class="info-item">
-                <b>Likes</b> <span class="info-item-api">${likes}</span>
-            </p>
+        return `<div class="photo-card">
+        <a href="${largeImageURL}"><img class="photo" src="${webformatURL}" alt="${tags}" title="${tags}" loading="lazy"/></a>
+         <div class="info">
             <p class="info-item">
-                <b>Views</b> <span class="info-item-api">${views}</span>  
-            </p>
-            <p class="info-item">
-                <b>Comments</b> <span class="info-item-api">${comments}</span>  
-            </p>
-            <p class="info-item">
-                <b>Downloads</b> <span class="info-item-api">${downloads}</span> 
-            </p>
+                 <b>Likes</b> <span class="info-item-api">${likes}</span>
+             </p>
+             <p class="info-item">
+                 <b>Views</b> <span class="info-item-api">${views}</span>  
+             </p>
+             <p class="info-item">
+                 <b>Comments</b> <span class="info-item-api">${comments}</span>  
+             </p>
+             <p class="info-item">
+                 <b>Downloads</b> <span class="info-item-api">${downloads}</span> 
+             </p>
         </div>
-    </div>`;
+        </div>`;
       }
     )
     .join('');
+
   gallery.insertAdjacentHTML('beforeend', markup);
-  gallerySimpleLightbox.refresh();
+  lightbox.refresh();
 }
